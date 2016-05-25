a6b62ed91791d79e1becfc682df4f35e025a9877
vidsecki <vid.secki@gmail.com>
2016-05-25 10:46:48 +0200

    Dodan ionic projekt

---
+++ FaksFood/www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+     
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('restavracijeCtrl', function($scope) {
+
+})
+   
+.controller('zemljevidCtrl', function($scope) {
+
+})
+   
+.controller('profilCtrl', function($scope) {
+
+})
+   
+.controller('restavracijaCtrl', function($scope) {
+
+})
+   
+.controller('meniCtrl', function($scope) {
+
+})
+   
+.controller('komentarjiCtrl', function($scope) {
+
+})
+   
+.controller('oceneCtrl', function($scope) {
+
+})
+ 
