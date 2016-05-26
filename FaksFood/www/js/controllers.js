c1ed6a611b8d5965355a3a07a5aaa993fd423678
Gregor Lenič <lenic4789@gmail.com>
2016-05-26 12:18:09 +0200

    neki neki neki

--- FaksFood/www/js/controllers.js
+++ FaksFood/www/js/controllers.js
@@ -8,29 +8,32 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('restavracijeCtrl', function($scope, filterFilter, Restavracije) {
+.controller('restavracijeCtrl', function($scope, $ionicScrollDelegate, filterFilter, Restavracije) {
 	var data = null;
     var ind = 0
 
-    Restavracije.getRestavracije().then(function(data){
-		$scope.restavracije = data;
+    Restavracije.getRestavracije().then(function(getdata){
+		data = getdata;
+        $scope.buffer = angular.copy(getdata);
+        $scope.restavracije = getdata.slice(0, 10)
 	});
-
     $scope.typed = function(searchText){
         $scope.buffer = filterFilter(data, searchText);
-        console.log("ASD");
     }
 
-    $scope.$watch('data', function(){
-        console.log('data changed')
-        ind = 0;
+    $scope.$watch('buffer', function(){
+        ind = 0
+        $scope.restavracije = $scope.buffer.slice(0, 10);
+        $ionicScrollDelegate.scrollTop();
     })
-	$scope.items = [];
     $scope.loadMore = function() {
-        $http.get('/more-items').success(function(items) {
-            useItems(items);
-            $scope.$broadcast('scroll.infiniteScrollComplete');
-        });
+        ind = ind + 10
+        var r = 10
+        if (ind + 10 >= $scope.buffer.length) {
+            r = $scope.buffer.length - ind
+        }
+        $scope.restavracije = $scope.restavracije.concat($scope.buffer.slice(ind, r + ind));
+        $scope.$broadcast('scroll.infiniteScrollComplete');
     };
 
     $scope.$on('$stateChangeSuccess', function() {
@@ -102,5 +105,11 @@ angular.module('app.controllers', [])
    
 .controller('oceneCtrl', function($scope) {
 
+})
+
+.controller('headerCtrl', function($scope){
+    $scope.refresh=function(){
+        
+    }
 })
  
